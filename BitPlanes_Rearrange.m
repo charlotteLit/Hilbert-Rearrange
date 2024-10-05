4c26dd7ec7ea22b8a8b08d99b97069d4f3db4976
charlotte_w <1539009015@qq.com>
2024-10-05 21:19:27 +0800

    Sort out the framework

--- BitPlanes_Rearrange.m
+++ BitPlanes_Rearrange.m
@@ -1,32 +1,30 @@
 function [origin_bits] = BitPlanes_Rearrange(Plane,Block_size,type)
-% ����˵��������BMPR�㷨������λƽ�����Plane
-% ���룺Plane��λƽ�����,Block_size���ֿ��С��,type�������з�ʽ��
-% �����origin_bits��������֮���λƽ���������
+
 
 [row,col] = size(Plane);
-m = floor(row/Block_size); %m*n�����ֿ�ĸ���
+m = floor(row/Block_size); 
 n = floor(col/Block_size);
-origin_bits = zeros(); %������¼λƽ�������еı�����
-num = 0; %����
-%-------------------�ֿ��ڰ��б������ֿ�䰴�б�������-�У�------------------%
-if type==0 %0��00
-    for i=1:m  %�ֿ�֮�䰴�б���
+origin_bits = zeros(); 
+num = 0; 
+
+if type==0 
+    for i=1:m  
         for j=1:n
-            begin_x = (i-1)*Block_size+1; %��Ӧ�ֿ����ʼ����
+            begin_x = (i-1)*Block_size+1; 
             begin_y = (j-1)*Block_size+1;
-            end_x = i*Block_size; %��Ӧ�ֿ�Ľ�������
+            end_x = i*Block_size; 
             end_y = j*Block_size;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
                 end
             end 
         end
-        if col-n*Block_size>=1  %��ʣ����
+        if col-n*Block_size>=1  
             begin_y = n*Block_size+1;
             end_y = col;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -34,13 +32,13 @@ if type==0 %0��00
             end
         end  
     end
-    if row-m*Block_size>=1 %��ʣ����
+    if row-m*Block_size>=1 
         begin_x = m*Block_size+1; 
         end_x = row;
         for j=1:n
             begin_y = (j-1)*Block_size+1;
             end_y = j*Block_size;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -48,37 +46,37 @@ if type==0 %0��00
             end  
         end  
     end
-    if row-m*Block_size>=1 && col-n*Block_size>=1 %����ʣ������
+    if row-m*Block_size>=1 && col-n*Block_size>=1 
         begin_x = m*Block_size+1;
         begin_y = n*Block_size+1;
         end_x = row;
         end_y = col;
-        for x=begin_x:end_x  %�ֿ�֮�䰴�б���
+        for x=begin_x:end_x  
             for y=begin_y:end_y
                 num = num+1;
                 origin_bits(num) = Plane(x,y);
             end
         end  
     end
-%-------------------�ֿ��ڰ��б������ֿ�䰴�б�������-�У�------------------%
-elseif type==1 %1��01
-    for j=1:n  %�ֿ�֮�䰴�б���
+
+elseif type==1 
+    for j=1:n  
         for i=1:m
-            begin_x = (i-1)*Block_size+1; %��Ӧ�ֿ����ʼ����
+            begin_x = (i-1)*Block_size+1; 
             begin_y = (j-1)*Block_size+1;
-            end_x = i*Block_size; %��Ӧ�ֿ�Ľ�������
+            end_x = i*Block_size; 
             end_y = j*Block_size;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
                 end
             end 
         end
-        if row-m*Block_size>=1  %��ʣ����
+        if row-m*Block_size>=1  
             begin_x = m*Block_size+1;
             end_x = row;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -86,13 +84,13 @@ elseif type==1 %1��01
             end
         end  
     end
-    if col-n*Block_size>=1 %��ʣ����
+    if col-n*Block_size>=1 
         begin_y = n*Block_size+1; 
         end_y = col;
         for i=1:m
             begin_x = (i-1)*Block_size+1;
             end_x = i*Block_size;
-            for x=begin_x:end_x  %�ֿ�֮�ڰ��б���
+            for x=begin_x:end_x  
                 for y=begin_y:end_y
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -100,37 +98,37 @@ elseif type==1 %1��01
             end  
         end  
     end
-    if row-m*Block_size>=1 && col-n*Block_size>=1 %����ʣ������
+    if row-m*Block_size>=1 && col-n*Block_size>=1 
         begin_x = m*Block_size+1;
         begin_y = n*Block_size+1;
         end_x = row;
         end_y = col;
-        for x=begin_x:end_x  %�ֿ�֮�䰴�б���
+        for x=begin_x:end_x  
             for y=begin_y:end_y
                 num = num+1;
                 origin_bits(num) = Plane(x,y);
             end
         end  
     end
-%-------------------�ֿ��ڰ��б������ֿ�䰴�б�������-�У�------------------%
-elseif type==2 %2��10
-    for i=1:m  %�ֿ�֮�䰴�б���
+
+elseif type==2 
+    for i=1:m  
         for j=1:n
-            begin_x = (i-1)*Block_size+1; %��Ӧ�ֿ����ʼ����
+            begin_x = (i-1)*Block_size+1; 
             begin_y = (j-1)*Block_size+1;
-            end_x = i*Block_size; %��Ӧ�ֿ�Ľ�������
+            end_x = i*Block_size; 
             end_y = j*Block_size; 
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
                 end
             end 
         end
-        if col-n*Block_size>=1  %��ʣ����
+        if col-n*Block_size>=1  
             begin_y = n*Block_size+1;
             end_y = col;
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -138,13 +136,13 @@ elseif type==2 %2��10
             end 
         end  
     end
-    if row-m*Block_size>=1 %��ʣ����
+    if row-m*Block_size>=1 
         begin_x = m*Block_size+1; 
         end_x = row;
         for j=1:n
             begin_y = (j-1)*Block_size+1;
             end_y = j*Block_size;
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -152,37 +150,37 @@ elseif type==2 %2��10
             end   
         end  
     end
-    if row-m*Block_size>=1 && col-n*Block_size>=1 %����ʣ������
+    if row-m*Block_size>=1 && col-n*Block_size>=1 
         begin_x = m*Block_size+1;
         begin_y = n*Block_size+1;
         end_x = row;
         end_y = col;
-        for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+        for y=begin_y:end_y  
             for x=begin_x:end_x  
                 num = num+1;
                 origin_bits(num) = Plane(x,y);
             end
         end   
     end
-%-------------------�ֿ��ڰ��б������ֿ�䰴�б�������-�У�------------------%
-elseif type==3 %type==3��3��11
-    for j=1:n  %�ֿ�֮�䰴�б���
+
+elseif type==3 
+    for j=1:n  
         for i=1:m
-            begin_x = (i-1)*Block_size+1; %��Ӧ�ֿ����ʼ����
+            begin_x = (i-1)*Block_size+1; 
             begin_y = (j-1)*Block_size+1;
-            end_x = i*Block_size; %��Ӧ�ֿ�Ľ�������
+            end_x = i*Block_size; 
             end_y = j*Block_size;
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
                 end
             end  
         end
-        if row-m*Block_size>=1  %��ʣ����
+        if row-m*Block_size>=1  
             begin_x = m*Block_size+1;
             end_x = row;
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -190,13 +188,13 @@ elseif type==3 %type==3��3��11
             end
         end  
     end
-    if col-n*Block_size>=1 %��ʣ����
+    if col-n*Block_size>=1 
         begin_y = n*Block_size+1; 
         end_y = col;
         for i=1:m
             begin_x = (i-1)*Block_size+1;
             end_x = i*Block_size;
-            for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+            for y=begin_y:end_y  
                 for x=begin_x:end_x  
                     num = num+1;
                     origin_bits(num) = Plane(x,y);
@@ -204,26 +202,26 @@ elseif type==3 %type==3��3��11
             end  
         end  
     end
-    if row-m*Block_size>=1 && col-n*Block_size>=1 %����ʣ������
+    if row-m*Block_size>=1 && col-n*Block_size>=1 
         begin_x = m*Block_size+1;
         begin_y = n*Block_size+1;
         end_x = row;
         end_y = col;
-        for y=begin_y:end_y  %�ֿ�֮�ڰ��б���
+        for y=begin_y:end_y  
             for x=begin_x:end_x  
                 num = num+1;
                 origin_bits(num) = Plane(x,y);
             end
         end  
     end 
-%-------------------hilbert����---------------------%
-else %type==4��4��hilbert
+
+else 
     coor = traverse(3);
-    l = floor(row/8); %m*n����8�ֿ�
+    l = floor(row/8); 
     w = floor(col/8);
     for i=1:l
         for j=1:w
-            begin_x = (i-1)*8+1; %��Ӧ�ֿ����ʼ����
+            begin_x = (i-1)*8+1; 
             begin_y = (j-1)*8+1;
             for c=1:2:127
                 x = coor(c)+begin_x;
--- README.md
+++ README.md
@@ -2,8 +2,26 @@
 ### 对源文件进行修改
 在源代码基础上，修改位重排方式
 
-四种排列方式：00，01，10，11
+四种排列方式：00,01,10,11
 ➡️
-Hilbert遍历，8*8大小的块
+Hilbert遍历8*8大小的块
 
 ### Hilbert遍历顺序
+
+### 函数说明
+- Vacate_Encrypt 空出图像空间，加密原始图像
+  - Prediction_Error 计算预测误差
+  - Vacate_Room 预留空间
+    - BitPlanes_Extract 提取位平面
+    - BitPlanes_Compress 位平面压缩
+      - BitPlanes_Rearrange 位平面重排
+      - BitStream_Compress 比特流重排
+    - BitPlanes_Embed 嵌入位平面
+  - Image_Shuffle 图像混洗
+- Data_Embed 数据嵌入
+  - Image_ReShuffle 图像反混洗
+  - Data_Encrypt 数据加密
+- Data_Extract 数据提取
+- Image_Recover 图像恢复
+  - BitStream_DeCompress 位平面解压缩
+  - BitPlanes_Recover 恢复位平面
